ec232ba9bd715dcaccf90b7667c12eb5785535b7
Kevin Yunas <kevinyunas@icloud.com>
2022-03-13 19:50:38 -0700

    Booted up integration with s3 using boto3

--- backend/app.py
+++ backend/app.py
@@ -1,16 +1,28 @@
 from dotenv import load_dotenv
-from flask import Flask
-import boto
+from flask import Flask, jsonify
+import boto3
 from boto.s3.key import Key
-
+from config import S3_BUCKET, S3_KEY, S3_SECRET
 from routes.images import images_route
 
-load_dotenv()
+print(S3_KEY, S3_SECRET)
+
+s3 = boto3.client('s3', 
+    aws_access_key_id=S3_KEY,
+    aws_secret_access_key=S3_SECRET)
 
 app = Flask(__name__)
-app.config['FLASKS3_BUCKET_NAME'] = 'https://pxclone-dev.s3.us-west-1.amazonaws.com/'
-s3 = FlaskS3(app)
 app.register_blueprint(images_route)
+
+@app.route('/files')
+def files():
+  s3_resource = boto3.resource('s3',
+    aws_access_key_id=S3_KEY,
+    aws_secret_access_key=S3_SECRET)
+  my_bucket = s3_resource.Bucket(S3_BUCKET)
+  for key in my_bucket.objects.all():
+    print(key)
+  return jsonify("files")
 
 if __name__ == "__main__":
   app.run()
