2647642274fd85f9325c856f084f98462fcc5962
Kevin Yunas <kevinyunas@icloud.com>
2022-04-04 11:32:46 -0700

    Created validator for post name uniquness for a given user

--- backend/controllers/validators/post_validator.py
+++ backend/controllers/validators/post_validator.py
@@ -1,7 +1,8 @@
 from importlib.metadata import requires
 from attr import field
-from marshmallow import Schema, fields
+from marshmallow import Schema, fields, ValidationError, validates_schema
 from marshmallow.validate import Length
+from ...db.models import Post
 
 class PostSchema(Schema):
   """
@@ -13,6 +14,16 @@ class PostSchema(Schema):
   - photodata (str)
   - description (str)
   """
+
+  @validates_schema
+  def validate_name_uniqueness_for_user(self, data, **kwargs):
+    name = data['name']
+    user_id = data['user_id']
+    post = Post.query().filter(Post.name == name).filter(Post.user_id == user_id). \
+      scalar()
+    if post is not None:
+      raise ValidationError("You have already created a post with the same name")
+
 
   user_id = fields.Int(required=True)
   name = fields.Str(required=True, validate=Length(min=1, max=64))
